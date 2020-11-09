38f55312d32b86580403e2937320cd8328b8c693
Ssegura09 <stephaniemsegura09@gmail.com>
2020-11-09 12:10:06 -0600

    updated without bugs

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,38 +1,10 @@
-
-import React, { Component } from 'react';
-import "./App.css";
-import Games from './components/Games'
-
-const URLBase = "http://localhost:3000/users/"
-
-class App extends Component {
-
-  // state = {
-  //   allUsers: []
-  // }
-
-  // componentDidMount() {
-  //   fetch(URLBase)
-  //   .then(res => res.json())
-  //   .then(user => {
-  //     this.setState({
-  //       allUsers: user
-  //     })
-  //   })
-  // }
-
-  render() {
-    return (
-      <div className="App">
-        <Games />
-      </div>
-
 import React, { Component } from "react";
 import "./App.css";
 import { BrowserRoute, BrowserRouter, Route, Link, Switch } from "react-router-dom";
 import SignUp from "./SignUp";
 import Login from "./Login";
 import Home from "./Home";
+import Games from './components/Games'
 const URLBase = "http://localhost:3000/users/";
 
 class App extends Component {
@@ -65,9 +37,16 @@ class App extends Component {
               <li>
                 <Link to="/signup">Sign Up</Link>
               </li>
+              <li>
+                <Link to="/components/games">Games</Link>
+              </li>
             </ul>
           </nav>
+          {/* <Games/> */}
           <Switch>
+            <Route exact path="/components/games">
+            <Games/>
+            </Route>
             <Route path="/login">
               <Login />
             </Route>
