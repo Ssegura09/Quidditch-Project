ac07540433fb4c863546ecd183e7c647f8cd9aab
Ssegura09 <stephaniemsegura09@gmail.com>
2020-11-08 21:57:32 -0600

    react routes

---
+++ backend/app/controllers/auth_controller.rb
@@ -0,0 +1,26 @@
+class Api::V1::AuthController < ApplicationController
+    # before_action :logged_in?
+    skip_before_action :logged_in?
+    def create
+        # authenticate the user
+        # 
+        user = User.find_by(username: params[:username]) 
+        if user && user.authenticate(params[:password])
+            # this block runs when:
+            # user exist and password matches the digest column in user table
+            # if authenticated, return JWT <<< user.id
+
+            # post lecture
+            # I NEED to pass a hash of the data we want to encode
+            # previously it was just the user.id, 
+            # make a hash with the key you want to assign and the value of whatever it is youre passing 
+            render json: {username: user.username, token: JWT.encode({user_id: user.id}, "texas")}
+
+        else
+            # if there is no user or password was incorrect
+            render json: {error: "invalid username or password"}
+
+        end
+    end
+
+end
---
+++ frontend/src/Home.js
@@ -0,0 +1,15 @@
+import React, { Component } from "react";
+
+class Home extends Component {
+  render() {
+    return (
+      <div>
+        <h3 className="welcome">Welcome To Quidditch!
+        <image src="https://fontmeme.com/permalink/201109/db5cdf9c5c4e61ed91ba345beed96d6c.png"/>
+        </h3>
+      </div>
+    );
+  }
+}
+
+export default Home;
