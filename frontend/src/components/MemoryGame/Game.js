8a2c7c5d98157b0cbf38f92852b9b642dff1b3c3
Ssegura09 <stephaniemsegura09@gmail.com>
2020-11-12 14:27:11 -0600

    styling

--- frontend/src/components/MemoryGame/Game.js
+++ frontend/src/components/MemoryGame/Game.js
@@ -4,7 +4,7 @@ import Images from "./Images";
 class Game extends React.Component {
   render() {
     return (
-      <div className="memory-background">
+      <div className="welcome-background">
         <div className="memory-container">
           <Images score={this.props.score} endGame={this.props.endGame} />
         </div>
